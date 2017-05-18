a60785027ac29337d1d258f799577ad7dfc02ee2
Sebastian Davids <sdavids@gmx.de>
2017-05-18 16:24:11 +0200

    MockServices
    Signed-off-by: Sebastian Davids <sdavids@gmx.de>

---
+++ src/main/java/io/sdavids/commons/test/MockServices.java
@@ -0,0 +1,245 @@
+/*
+ * Copyright (c) 2017, Sebastian Davids
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.sdavids.commons.test;
+
+import static java.lang.String.format;
+import static java.lang.reflect.Modifier.isAbstract;
+import static java.lang.reflect.Modifier.isPublic;
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static java.util.Arrays.stream;
+import static java.util.Collections.unmodifiableSet;
+import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.toSet;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.lang.invoke.MethodHandles;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLStreamHandler;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.Locale;
+import java.util.Set;
+
+/**
+ * Enables one to register mock services.
+ *
+ * <h3 id="usage">Usage</h3>
+ *
+ * <pre><code>
+ * &#64;Before
+ * public void setUp() {
+ *   MockServices.setServices(MyServiceMock.class);
+ * }
+ *
+ * &#64;Test
+ * public void getService() {
+ *   Iterator&lt;MyService&gt; providers = ServiceLoader.load(MyService.class).iterator();
+ *
+ *   // providers.next() is MyServiceMock
+ * }
+ * </code></pre>
+ *
+ * @see java.util.ServiceLoader
+ * @since 1.0
+ */
+public final class MockServices {
+
+  @SuppressWarnings("CustomClassloader")
+  private static final class ServiceClassLoader extends ClassLoader {
+
+    @SuppressWarnings("HardcodedFileSeparator")
+    private static final String META_INF_SERVICES = "META-INF/services/";
+
+    private final Set<Class<?>> services;
+
+    ServiceClassLoader(Class<?>... services) {
+      super(MethodHandles.lookup().lookupClass().getClassLoader());
+
+      Set<Class<?>> classes = stream(services).collect(toSet());
+
+      for (Class<?> clazz : classes) {
+        try {
+          int mods = clazz.getModifiers();
+          if (!isPublic(mods) || isAbstract(mods)) {
+            throw new IllegalArgumentException(
+                format(Locale.ROOT, "Class %s must be public", clazz.getName()));
+          }
+          clazz.getConstructor();
+        } catch (NoSuchMethodException e) {
+          throw new IllegalArgumentException(
+              format(Locale.ROOT, "Class %s has no public no-arg constructor", clazz.getName()), e);
+        }
+      }
+
+      this.services = unmodifiableSet(classes);
+    }
+
+    @Override
+    public URL getResource(String name) {
+      Enumeration<URL> r;
+      try {
+        r = getResources(name);
+      } catch (IOException ignored) {
+        return null;
+      }
+      return r.hasMoreElements() ? r.nextElement() : null;
+    }
+
+    @SuppressWarnings("OverlyBroadThrowsClause")
+    @Override
+    public Enumeration<URL> getResources(String name) throws IOException {
+      Enumeration<URL> resources = super.getResources(name);
+
+      if (!name.startsWith(META_INF_SERVICES)) {
+        return resources;
+      }
+
+      Class<?> serviceClass;
+      try {
+        serviceClass = loadClass(name.substring(META_INF_SERVICES.length()));
+      } catch (ClassNotFoundException ignored) {
+        return resources;
+      }
+
+      Collection<String> impls =
+          services
+              .stream()
+              .filter(serviceClass::isAssignableFrom)
+              .map(Class::getName)
+              .collect(toSet());
+
+      if (impls.isEmpty()) {
+        return resources;
+      }
+
+      try (ByteArrayOutputStream baos = new ByteArrayOutputStream();
+          PrintWriter pw = new PrintWriter(new OutputStreamWriter(baos, UTF_8))) {
+        for (String impl : impls) {
+          pw.println(impl);
+          pw.println("#position=100");
+        }
+        pw.flush();
+        return new ServiceEnumeration(resources, serviceClass, baos);
+      }
+    }
+  }
+
+  private static final class ServiceEnumeration implements Enumeration<URL> {
+
+    private final Enumeration<URL> resources;
+    private final URL url;
+
+    private boolean parent;
+
+    ServiceEnumeration(
+        Enumeration<URL> resources, Class<?> serviceClass, ByteArrayOutputStream baos)
+        throws MalformedURLException {
+
+      this.resources = resources;
+      url =
+          new URL(
+              "metainfservices", null, 0, serviceClass.getName(), new ServiceStreamHandler(baos));
+    }
+
+    @Override
+    public boolean hasMoreElements() {
+      return !parent || resources.hasMoreElements();
+    }
+
+    @Override
+    public URL nextElement() {
+      if (parent) {
+        return resources.nextElement();
+      } else {
+        parent = true;
+        return url;
+      }
+    }
+  }
+
+  private static final class ServiceStreamHandler extends URLStreamHandler {
+
+    @SuppressWarnings("PackageVisibleField")
+    final byte[] bytes;
+
+    ServiceStreamHandler(ByteArrayOutputStream baos) {
+      bytes = baos.toByteArray();
+    }
+
+    @Override
+    protected URLConnection openConnection(URL u) {
+      //noinspection AnonymousInnerClass,ReturnOfInnerClass,AnonymousInnerClassWithTooManyMethods,InnerClassTooDeeplyNested
+      return new URLConnection(u) {
+
+        @Override
+        public void connect() {}
+
+        @Override
+        public InputStream getInputStream() {
+          return new ByteArrayInputStream(bytes);
+        }
+      };
+    }
+  }
+
+  /**
+   * Sets (or resets) the mock services.
+   *
+   * <p>Clears any previous mock service registrations.
+   *
+   * <p>Service implementations registered via {@code META-INF/services/} are available after the
+   * ones registered by this method.
+   *
+   * <p>Each mock service class must be public and have a public no-arg constructor.
+   *
+   * @param services the mock services, not null
+   * @since 1.0
+   */
+  public static void setServices(Class<?>... services) {
+    requireNonNull(services, "services");
+
+    //noinspection ClassLoaderInstantiation
+    ClassLoader loader = new ServiceClassLoader(services);
+
+    ThreadGroup threadGroup = Thread.currentThread().getThreadGroup();
+    //noinspection MethodCallInLoopCondition
+    while (threadGroup.getParent() != null) {
+      threadGroup = threadGroup.getParent();
+    }
+
+    while (true) {
+      int count = threadGroup.activeCount() + 1;
+      //noinspection ObjectAllocationInLoop
+      Thread[] threads = new Thread[count];
+      int n = threadGroup.enumerate(threads, true);
+      if (n < count) {
+        for (int i = 0; i < n; i++) {
+          threads[i].setContextClassLoader(loader);
+        }
+        break;
+      }
+    }
+  }
+
+  private MockServices() {}
+}
---
+++ src/test/java/io/sdavids/commons/test/MockServicesTest.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2017, Sebastian Davids
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.sdavids.commons.test;
+
+import static java.util.ServiceLoader.load;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Iterator;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public final class MockServicesTest {
+
+  private static <T> T getServiceInterface(Class<T> clazz) {
+    Iterator<T> providers = load(clazz).iterator();
+
+    return providers.hasNext() ? providers.next() : null;
+  }
+
+  @Rule public ExpectedException expectedException = ExpectedException.none();
+
+  @Test
+  public void setServices_null() {
+    expectedException.expect(NullPointerException.class);
+    expectedException.expectMessage("services");
+
+    //noinspection ConstantConditions
+    MockServices.setServices((Class<?>[]) null);
+
+    assertThat(getServiceInterface(ServiceInterface1.class)).isNull();
+    assertThat(getServiceInterface(ServiceInterface2.class)).isNull();
+  }
+
+  @Test
+  public void setServices_non_public_no_arg_ctor() {
+    expectedException.expect(IllegalArgumentException.class);
+    expectedException.expectMessage(
+        "Class io.sdavids.commons.test.NonPublicNoArgCtorServiceInterface1 has no public no-arg constructor");
+
+    MockServices.setServices(NonPublicNoArgCtorServiceInterface1.class);
+  }
+
+  @Test
+  public void setServices_abstract() {
+    expectedException.expect(IllegalArgumentException.class);
+    expectedException.expectMessage(
+        "Class io.sdavids.commons.test.AbstractServiceInterface1 must be public");
+
+    MockServices.setServices(AbstractServiceInterface1.class);
+  }
+
+  @Test
+  public void setServices_empty() {
+    MockServices.setServices();
+
+    assertThat(getServiceInterface(ServiceInterface1.class)).isNull();
+    assertThat(getServiceInterface(ServiceInterface2.class)).isNull();
+  }
+
+  @Test
+  public void setServices_one() {
+    MockServices.setServices(TestableServiceInterface1.class);
+
+    ServiceInterface1 serviceInterface = getServiceInterface(ServiceInterface1.class);
+
+    assertThat(serviceInterface).isNotNull();
+    assertThat(serviceInterface).isInstanceOf(TestableServiceInterface1.class);
+    assertThat(serviceInterface.value()).isEqualTo(1);
+
+    assertThat(getServiceInterface(ServiceInterface2.class)).isNull();
+  }
+
+  @Test
+  public void setServices_several() {
+    MockServices.setServices(TestableServiceInterface2.class, TestableServiceInterface1.class);
+
+    ServiceInterface1 serviceInterface1 = getServiceInterface(ServiceInterface1.class);
+
+    assertThat(serviceInterface1).isNotNull();
+    assertThat(serviceInterface1).isInstanceOf(TestableServiceInterface1.class);
+    assertThat(serviceInterface1.value()).isEqualTo(1);
+
+    ServiceInterface2 serviceInterface2 = getServiceInterface(ServiceInterface2.class);
+
+    assertThat(serviceInterface2).isNotNull();
+    assertThat(serviceInterface2).isInstanceOf(ServiceInterface2.class);
+    assertThat(serviceInterface2.value()).isEqualTo(2);
+  }
+}
